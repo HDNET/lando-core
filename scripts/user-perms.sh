f3bd0ef3643bb886ed5a6f5ac3fe2a167303e9b1
Florian Patruck <florian.patruck@hdnet.de>
2024-12-29 13:50:59 +0100

    fix(perm-helpers): Fix permission setup so that it also works for alpine containers without bash
    fix(perms): Do not chown mounted volumes at all because docker itself smart enough now to handle this
    
    perf: Do not chown bind mounts inside /var/www as they are right anyway
    
    Simplify user-perms by installing shadow into alpine and just having one code path

--- scripts/user-perms.sh
+++ scripts/user-perms.sh
@@ -26,7 +26,6 @@ fi
 : ${LANDO_WEBROOT_GROUP:='www-data'}
 : ${LANDO_WEBROOT_UID:=$(id -u $LANDO_WEBROOT_USER 2>/dev/null)}
 : ${LANDO_WEBROOT_GID:=$(id -g $LANDO_WEBROOT_GROUP 2>/dev/null)}
-: ${LANDO_ADDUSER_EXTRAS:='-M -N'}
 
 # Get the linux flavor
 if [ -f /etc/os-release ]; then
@@ -47,32 +46,15 @@ else
   FLAVOR="debian"
 fi
 
+if [ "$FLAVOR" = "alpine" ] && ! command -v usermod > /dev/null 2>&1; then
+  lando_info "Alpine detected and shadow not found, installing shadow utils for usermod/groupmod..."
+  apk add --no-cache shadow
+fi
+
 # Make things
 mkdir -p /var/www/.ssh
 mkdir -p /user/.ssh
 mkdir -p /app
-
-# Get the webroot user's home directory
-WEBROOT_HOME=$(getent passwd "$LANDO_WEBROOT_USER" | cut -d : -f 6)
-if [ -z "$WEBROOT_HOME" ]; then
-  WEBROOT_HOME="/var/www"
-fi
-
-lando_info "meUsers home directory: $WEBROOT_HOME"
-
-# Symlink the gitconfig
-if [ -f "/user/.gitconfig" ] && [ ! -f "$WEBROOT_HOME/.gitconfig" ]; then
-  mkdir -p "$WEBROOT_HOME"
-  ln -sf /user/.gitconfig "$WEBROOT_HOME/.gitconfig"
-  lando_info "Symlinked users .gitconfig."
-fi
-
-# Symlink the known_hosts
-if [ -f "/user/.ssh/known_hosts" ] && [ ! -f "$WEBROOT_HOME/.ssh/known_hosts" ]; then
-  mkdir -p "$WEBROOT_HOME/.ssh"
-  ln -sf /user/.ssh/known_hosts "$WEBROOT_HOME/.ssh/known_hosts"
-  lando_info "Symlinked users known_hosts"
-fi
 
 if [ ! -z ${LANDO_NO_USER_PERMS+x} ]; then
   lando_info "Skipping user perm sweep at because LANDO_NO_USER_PERMS is set"
@@ -94,19 +76,37 @@ lando_debug "LANDO_HOST_GID          : $LANDO_HOST_GID"
 lando_debug "========================================"
 lando_debug ""
 
-# Adding user if needed
-lando_info "Making sure correct user:group ($LANDO_WEBROOT_USER:$LANDO_WEBROOT_GROUP) exists..."
-add_user $LANDO_WEBROOT_USER $LANDO_WEBROOT_GROUP $LANDO_WEBROOT_UID $LANDO_WEBROOT_GID $FLAVOR "$LANDO_ADDUSER_EXTRAS"
-verify_user $LANDO_WEBROOT_USER $LANDO_WEBROOT_GROUP $FLAVOR
-
 # Correctly map users
 # Lets do this regardless of OS now
 lando_info "Remapping ownership to handle docker volume sharing..."
 lando_info "Resetting $LANDO_WEBROOT_USER:$LANDO_WEBROOT_GROUP from $LANDO_WEBROOT_UID:$LANDO_WEBROOT_GID to $LANDO_HOST_UID:$LANDO_HOST_GID"
-reset_user $LANDO_WEBROOT_USER $LANDO_WEBROOT_GROUP $LANDO_HOST_UID $LANDO_HOST_GID $FLAVOR
+reset_user $LANDO_WEBROOT_USER $LANDO_WEBROOT_GROUP $LANDO_HOST_UID $LANDO_HOST_GID
 lando_info "$LANDO_WEBROOT_USER:$LANDO_WEBROOT_GROUP is now running as $(id $LANDO_WEBROOT_USER)!"
+verify_user $LANDO_WEBROOT_USER $LANDO_WEBROOT_GROUP
+
+# Get the webroot user's home directory
+WEBROOT_HOME=$(getent passwd "$LANDO_WEBROOT_USER" | cut -d : -f 6)
+if [ -z "$WEBROOT_HOME" ]; then
+  WEBROOT_HOME="/var/www"
+fi
+lando_info "meUsers home directory: $WEBROOT_HOME"
+
+# Symlink the gitconfig
+if [ -f "/user/.gitconfig" ] && [ ! -f "$WEBROOT_HOME/.gitconfig" ]; then
+  mkdir -p "$WEBROOT_HOME"
+  ln -sf /user/.gitconfig "$WEBROOT_HOME/.gitconfig"
+  lando_info "Symlinked users .gitconfig."
+fi
+
+# Symlink the known_hosts
+if [ -f "/user/.ssh/known_hosts" ] && [ ! -f "$WEBROOT_HOME/.ssh/known_hosts" ]; then
+  mkdir -p "$WEBROOT_HOME/.ssh"
+  ln -sf /user/.ssh/known_hosts "$WEBROOT_HOME/.ssh/known_hosts"
+  lando_info "Symlinked users known_hosts"
+fi
 
 # Make sure we set the ownership of the mount and HOME when we start a service
 lando_info "And here. we. go."
 lando_info "Doing the permission sweep."
 perm_sweep $LANDO_WEBROOT_USER $(getent group "$LANDO_HOST_GID" | cut -d: -f1) $WEBROOT_HOME $LANDO_RESET_DIR
+
