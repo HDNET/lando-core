cd85e40dd8d54dadab4bbab115fe9b1f2342179b
Florian Patruck <florian.patruck@hdnet.de>
2026-01-30 16:35:55 +0100

    fix(tooling): fix resolve dir/appmount on the first start if we start with the app bootstrap and no compose cache is there. How creazy is that?

--- hooks/app-add-tooling.js
+++ hooks/app-add-tooling.js
@@ -8,6 +8,25 @@ module.exports = async (app, lando) => {
 
     // Add the tasks after we init the app
     _.forEach(require('../utils/get-tooling-tasks')(app.config.tooling, app), task => {
+      const service = task.service;
+      // ensure all v3 services have their appMount set to /app
+      const v3Mounts = _(_.get(app, 'info', []))
+        .filter(service => service.api !== 4)
+        .map(service => ([service.service, service.appMount || '/app']))
+        .fromPairs()
+        .value();
+      app.mounts = _.merge({}, v3Mounts, app.mounts);
+
+      // mix in mount if applicable
+      if (!task.dir && _.has(app, `mounts.${service}`)) task.appMount = app.mounts[service];
+
+      // and working dir data if no dir or appMount
+      if (!task.dir) {
+        const sconf = _.get(app, `config.services.${service}`, {});
+        const workdir = sconf?.overrides?.working_dir ?? sconf?.working_dir;
+        if (workdir) task.dir = app.config.services[service].working_dir;
+      }
+
       app.log.debug('adding app cli task %s', task.name);
       const injectable = _.has(app, 'engine') ? app : lando;
       app.tasks.push(require('../utils/build-tooling-task')(task, injectable));
