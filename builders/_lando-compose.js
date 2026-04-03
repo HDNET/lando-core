910dad00696750591a4fc6a540ff8419914b7325
Florian Patruck <florian.patruck@hdnet.de>
2026-04-03 22:25:41 +0200

    feat: Set the entrypoint to /lando-entrypoint.sh but make sure that the existing entrypoint/command pair is preserved so that you do not have to do that yourself
    Think about doing this as well for the appMount
    The meUser does not make sense I guess

--- builders/_lando-compose.js
+++ builders/_lando-compose.js
@@ -2,19 +2,43 @@
 
 const _ = require('lodash');
 
+const toArray = value => Array.isArray(value) ? value : [];
+
 module.exports = {
   name: '_lando-compose',
   parent: '_lando',
   builder: parent => class LandoComposeServiceV3 extends parent {
     constructor(id, options = {}) {
-      super(id, _.merge({}, {
-        entrypoint: null, // NOTE: Do not overwrite the entrypoint from docker compose. Or should we?
+      // get the original entrypoint and command from the compose data
+      // load-compose-files ensures these are populated from the image if not set in compose
+      const composeServices = _.get(options, '_app.composeData[0].data[0].services', {});
+      const composeService = _.get(composeServices, options.name, {});
+
+      const originalEntrypoint = toArray(composeService.entrypoint);
+      const originalCommand = toArray(composeService.command);
+
+      if (originalEntrypoint.length === 1 &&
+        (originalEntrypoint[0] === '/lando-entrypoint.sh' || originalEntrypoint[0] === '/helpers/lando-entrypoint.sh')
+      ) {
+        options.landoEntrypoint = false;
+      }
+      const command = [...originalEntrypoint, ...originalCommand];
+
+      const opts = _.merge({}, {
+        // let the parent _lando set entrypoint to /lando-entrypoint.sh
         data: null, // NOTE: Do not create the data volume
         dataHome: null, // NOTE: Do not create the dataHome volume
-        meUser: 'root',
         appMount: '/',
         sslExpose: false,
-      }, options));
+      }, options);
+
+      if ((options.landoEntrypoint ?? true) && command.length > 0) {
+        opts.overrides = _.merge({}, {command}, options.overrides);
+      } else {
+        opts.entrypoint = undefined;
+      }
+
+      super(id, opts);
     }
   },
 };
